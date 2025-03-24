847e41355685fc02f34158d6bdf8c02bc21d878e
jgyudo <142569583+jgyudo@users.noreply.github.com>
2025-03-24 19:41:29 +0900

    Update 16564.py

--- JGD/week2/16564.py
+++ JGD/week2/16564.py
@@ -1,3 +1,9 @@
+# mid값에서 각 캐릭터의 레벨을 빼서 올릴 수 있는 level을 설정
+# level과 k를 비교
+# level이 k보다 작거나 같으면 level을 더 올릴 수 있는 경우의 수 존재 -> start 수정
+# level이 k보다 크면 level을 더 올릴 수 있는 경우의 수 x -> end를 수정하면서 mid의 최대값 찾기
+# 결국 반복문이 끝났을 때 mid값이 목표값이 됨
+
 import sys
 
 n,k = map(int,sys.stdin.readline().split())
