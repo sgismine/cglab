7ec216f5b2c15a7b82a39ddcb041fcbcbdd20568
노신비 <nsbee7@naver.com>
2025-05-19 02:44:11 +0900

    250519

--- NSB/5주차/스택/2493.py
+++ NSB/5주차/스택/2493.py
@@ -1,4 +1,5 @@
 # 2493 4번
+# 스택 안써서 풀었는데 시간초과 걸려서 풀이 참고했습니다..
 
 import sys
 N = int(input())
@@ -6,7 +7,6 @@ top = list(map(int,sys.stdin.readline().split()))
 
 stack = []
 ans = [0] * N
-
 
 for i in range(N):
     while stack :
