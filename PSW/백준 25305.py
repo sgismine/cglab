cc2e0d1f99a63fb09331d13decfe31ce125163c6
박선우 <113815235+sgismine@users.noreply.github.com>
2025-06-02 20:13:03 +0900

    Delete PSW/백준 25305.py

--- PSW/백준 25305.py
+++
@@ -1,6 +0,0 @@
-n,k = map(int,input().split())     # 응시자수, 상받는 사람 수 각각 
-score = list(map(int,input().split()))  # 스코어 각각 공백을 기준으로 나누기
-
-score.sort(reverse=True) #내림차순 정렬
-print(score[k-1]) # 커트라인 출력 -1 하는 이유는 0부터 리스트가 시작하기 때문
- 
