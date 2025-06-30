fbe96e15fd2150af452ae306b5d0a1f93efeae1a
박선우 <113815235+sgismine@users.noreply.github.com>
2025-06-30 20:15:35 +0900

    Update #1260.py

--- PSW/week9/#1260.py
+++ PSW/week9/#1260.py
@@ -22,25 +22,3 @@ def bfs(graph, start):
                 queue.append(neighbor)
     return result
 
-# 입력 받기
-n, m, v = map(int, input().split())  # 정점 수, 간선 수, 시작 정점
-graph = [[] for _ in range(n + 1)]  # 인접 리스트로 그래프 구현
-
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-
-# 인접 리스트 정렬 (작은 번호 우선 방문을 위해)
-for neighbors in graph:
-    neighbors.sort()
-
-# DFS 수행
-visited_dfs = [False] * (n + 1)
-result = []
-dfs(graph, v, visited_dfs)
-print(*result)
-
-# BFS 수행
-result = bfs(graph, v)
-print(*result)
